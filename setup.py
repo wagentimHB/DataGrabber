3e441cb246762b81fb96674344a2462fb1d70bda
huang.bin <bin.huang@porsche.de>
2024-06-25 16:46:41 +0200

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as f:
-    install_requires=f.read().splitlines()
+# Function to read requirements.txt
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+        return [line for line in lines if line and not line.startswith('#')]
 
-setuptools.setup(name='DataGrabber', packages='DataGrabber', install_requires=install_requires)
+setup(
+    name='DataGrabber',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+)
