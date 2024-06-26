7528354f5be67904621d38cb7de77d4fb6cf597a
huang.bin <bin.huang@porsche.de>
2024-06-26 15:24:20 +0200

    create basic structure

---
+++ test_selenium.py
@@ -0,0 +1,17 @@
+import unittest
+from driver.selenium import Selenium
+
+class TestSelenium(unittest.TestCase):
+
+    def test_initialBrowser(self):
+        obj = Selenium()
+
+        with self.assertRaises(ValueError):
+            obj.initial_browser("")
+
+        self.assertEqual(obj.initial_browser("firefox"), "firefox")
+        self.assertEqual(obj.initial_browser("edge"), "edge")
+        self.assertEqual(obj.initial_browser("chrome"), "chrome")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ worker/worker.py
@@ -0,0 +1,7 @@
+
+class Worker:
+
+    def __init__(self) -> None:
+        pass
+
+    
